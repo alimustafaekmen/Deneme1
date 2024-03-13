02a53f2fb4f1facfd9b23c3e8225d22aa97ba52b
ekmen <ekmen@DESKTOP-04I3GQG>
2024-03-13 13:51:44 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Deneme1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.GradientInactiveCaption;
+            button1.Font = new Font("Segoe UI", 18F);
+            button1.Location = new Point(287, 202);
+            button1.Name = "button1";
+            button1.Size = new Size(561, 86);
+            button1.TabIndex = 0;
+            button1.Text = "PERSONEL GİRİŞİ";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.GradientInactiveCaption;
+            button2.Font = new Font("Segoe UI", 18F);
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(287, 320);
+            button2.Name = "button2";
+            button2.Size = new Size(561, 86);
+            button2.TabIndex = 1;
+            button2.Text = "MÜŞTERİ GİRİŞİ";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.White;
+            label1.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label1.ForeColor = Color.Green;
+            label1.ImageAlign = ContentAlignment.BottomCenter;
+            label1.Location = new Point(363, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(418, 67);
+            label1.TabIndex = 2;
+            label1.Text = "GÜVEN OTOPARK";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.MenuBar;
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(1139, 639);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Cursor = Cursors.Default;
+            ForeColor = SystemColors.ActiveCaptionText;
+            Name = "Form1";
+            Text = "Login Sayfası";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Deneme1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 30F);
+            label1.Location = new Point(384, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(360, 67);
+            label1.TabIndex = 0;
+            label1.Text = "Personel Ekranı";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 18F);
+            button1.Location = new Point(282, 164);
+            button1.Name = "button1";
+            button1.Size = new Size(576, 74);
+            button1.TabIndex = 1;
+            button1.Text = "ARAÇ KAYIT";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 18F);
+            button2.Location = new Point(282, 244);
+            button2.Name = "button2";
+            button2.Size = new Size(576, 74);
+            button2.TabIndex = 2;
+            button2.Text = "ARAÇ ÇIKIŞ";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 18F);
+            button3.Location = new Point(282, 324);
+            button3.Name = "button3";
+            button3.Size = new Size(576, 74);
+            button3.TabIndex = 3;
+            button3.Text = "OTOPARK DURUMU SORGULAMA";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 18F);
+            button4.Location = new Point(282, 404);
+            button4.Name = "button4";
+            button4.Size = new Size(576, 74);
+            button4.TabIndex = 4;
+            button4.Text = "OTOPARK HASILATI";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1139, 639);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
